43dd7469517b0685d13a90143fa8881d1866222e
wagnersistemalima <wagner.sistemalima@gmail.com>
2023-04-24 12:02:35 -0300

    criacao da base de um projeto utilizando java com spring e kafka para consume e producao de mensagens

--- src/main/java/com/sistemalima/demo/KafkademoApplication.java
+++ src/main/java/com/sistemalima/demo/KafkademoApplication.java
@@ -1,10 +1,12 @@
 package com.sistemalima.demo;
 
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
+@EnableKafka
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class) // Needed by Zalando Problem lib
 public class KafkademoApplication {
